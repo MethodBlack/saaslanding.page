15cbf9a461bdb52a3a2318ed354700ca1c51fae3
planetabhi <abhimanyu.rana@postman.com>
2024-06-17 12:00:24 +0530

    setup

---
+++ src/persona.js
@@ -0,0 +1,53 @@
+export const persona = {
+    firstName: "SaaS",
+    secondName: "Landing",
+    description: "Celebrating the craft and care of SaaS marketing design",
+    story:
+      "Here, we celebrate exceptional SaaS landing pages. These carefully designed examples are more than marketing tools. They combine dedication, creativity, and precision, turning digital experiences into art. Discover the thoughtful use of typography, well-balanced layouts, and sticky details that showcase the power of thoughtful design. Let these pages inspire you to create your own exquisitely crafted and impactful landing pages.",
+  sections: {
+    books: [
+        {
+            title: "lovi.care",
+            desc: 'Digital Skincare',
+            url: 'https://lovi.care/?ref=saaslanding.page'
+        },
+        {
+          title: "dovetail.com",
+          desc: 'Dovetail Research',
+          url: 'https://dovetail.com/?ref=saaslanding.page'
+        },
+        {
+          title: "family.co",
+          desc: 'Family Wallet',
+          url: 'https://family.co/?ref=saaslanding.page'
+        },
+        {
+          title: "anygood.app",
+          desc: 'Konsider',
+          url: 'https://www.anygood.app/?ref=saaslanding.page'
+        },
+        {
+          title: "osome.com",
+          desc: 'Osome',
+          url: 'https://osome.com/?ref=saaslanding.page'
+        },
+        {
+          title: "refrakt.app",
+          desc: 'Refrakt',
+          url: 'https://refrakt.app/?ref=saaslanding.page'
+        },
+        {
+          title: "steep.app",
+          desc: 'Steep Analytics',
+          url: 'https://steep.app/?ref=saaslanding.page'
+        },                                    
+    ],
+  },
+      contacts: [
+      {
+        title: "© 2024 Method Black.",
+      },
+    ],
+
+  };
+
